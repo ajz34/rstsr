47c78af30174ae75a8b7e4b89564aac3c5ac0a64
ajz34 <ajz34@outlook.com>
2024-09-02 11:54:06 +0800

    drop cuda code for now

--- src/cuda_backend/error.rs
+++
@@ -1,8 +0,0 @@
-/// cudarc related errors
-#[derive(thiserror::Error, Debug)]
-pub enum CudaError {
-    #[error(transparent)]
-    CudaDriver(#[from] cudarc::driver::result::DriverError),
-}
-
-pub use CudaError::CudaDriver as CudaDriverError;
