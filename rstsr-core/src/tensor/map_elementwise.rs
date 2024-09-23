d24802f7146bcce5add3e0d08505a0a94bd2fb5b
ajz34 <ajz34@outlook.com>
2024-09-23 16:37:18 +0800

    map_binary

--- rstsr-core/src/tensor/map_elementwise.rs
+++ rstsr-core/src/tensor/map_elementwise.rs
@@ -62,6 +62,80 @@ where
     {
         self.map_inplace(move |x| *x = f(x.clone()));
     }
+
+    pub fn map_binary<'a, R2, T2, D2, DOut, TOut, F>(
+        &'a self,
+        other: &TensorBase<R2, D2>,
+        f: F,
+    ) -> Tensor<TOut, DOut, B>
+    where
+        R2: DataAPI<Data = Storage<T2, B>>,
+        D2: DimAPI,
+        DOut: DimAPI,
+        D: DimMaxAPI<D2, Max = DOut>,
+        T2: Clone,
+        TOut: Clone,
+        F: Fn(&T, &T2) -> TOut + 'a,
+        B: DeviceAPI<TOut> + DeviceCreationAnyAPI<TOut>,
+        B: DeviceOp_MutC_RefA_RefB_API<T, T2, TOut, DOut, dyn FnMut(&mut TOut, &T, &T2) + 'a>,
+    {
+        // get tensor views
+        let a = self.view();
+        let b = other.view();
+        // check device and layout
+        rstsr_assert!(a.device().same_device(b.device()), DeviceMismatch).unwrap();
+        let la = a.layout();
+        let lb = b.layout();
+        let (la_b, lb_b) = broadcast_layout(la, lb).unwrap();
+        // generate output layout
+        let lc_from_a = layout_for_array_copy(&la_b, TensorIterOrder::default()).unwrap();
+        let lc_from_b = layout_for_array_copy(&lb_b, TensorIterOrder::default()).unwrap();
+        let lc = if lc_from_a == lc_from_b {
+            lc_from_a
+        } else {
+            match TensorOrder::default() {
+                TensorOrder::C => la_b.shape().c(),
+                TensorOrder::F => la_b.shape().f(),
+            }
+        };
+        let device = self.device();
+        let mut storage_c = unsafe { device.empty_impl(lc.bounds_index().unwrap().1).unwrap() };
+        let storage_a = self.data().storage();
+        let storage_b = other.data().storage();
+        let mut f_inner = move |c: &mut TOut, a: &T, b: &T2| *c = f(a, b);
+        device
+            .op_mutc_refa_refb_func(
+                &mut storage_c,
+                &lc,
+                storage_a,
+                &la_b,
+                storage_b,
+                &lb_b,
+                &mut f_inner,
+            )
+            .unwrap();
+        return Tensor::new(DataOwned::from(storage_c), lc).unwrap();
+    }
+
+    pub fn mapv_binary<'a, R2, T2, D2, DOut, TOut, F>(
+        &'a self,
+        other: &TensorBase<R2, D2>,
+        f: F,
+    ) -> Tensor<TOut, DOut, B>
+    where
+        R2: DataAPI<Data = Storage<T2, B>>,
+        D2: DimAPI,
+        DOut: DimAPI,
+        D: DimMaxAPI<D2, Max = DOut>,
+        T: Clone,
+        T2: Clone,
+        TOut: Clone,
+        F: Fn(T, T2) -> TOut + 'a,
+        B: DeviceAPI<TOut> + DeviceCreationAnyAPI<TOut>,
+        B: DeviceOp_MutC_RefA_RefB_API<T, T2, TOut, DOut, dyn FnMut(&mut TOut, &T, &T2) + 'a>,
+    {
+        self.map_binary(other, move |x, y| f(x.clone(), y.clone()))
+    }
 }
 
 impl<T, D, B> Tensor<T, D, B>
@@ -92,9 +166,19 @@ mod tests {
 
     #[test]
     fn test_mapv() {
-        let f = |x: i32| x * 2;
-        let a = Tensor::from(vec![1, 2, 3, 4]);
+        let f = |x| x * 2.0;
+        let a = Tensor::from(vec![1., 2., 3., 4.]);
         let b = a.mapv(f);
+        assert!(allclose_f64(&b, &vec![2., 4., 6., 8.].into()));
         println!("{:?}", b);
+    }
+
+    #[test]
+    fn test_mapv_binary() {
+        let f = |x, y| 2.0 * x + 3.0 * y;
+        let a = Tensor::linspace_cpu(1., 6., 6).into_shape_assume_contig([2, 3]).unwrap();
+        let b = Tensor::linspace_cpu(1., 3., 3);
+        let c = a.mapv_binary(&b, f);
+        assert!(allclose_f64(&c, &vec![5., 10., 15., 11., 16., 21.].into()));
     }
 }
